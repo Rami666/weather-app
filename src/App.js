ee3e29a86b998e9836f20e9b1fce259cff73c2cc
Rami B <ramibasheer9@gmail.com>
2022-12-05 22:12:01 +0100

    apply changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=2b25ef353ef976c78d3f10e306dc9195`
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Dallas</p>
+          </div>
+          <div className="temp">
+            <h1>60°F</h1>
+          </div>
+          <div className="description">
+            <p>Cloudy</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>65°F</p>
+          </div>
+          <div className="humidity">
+            <p>65%</p>
+          </div>
+          <div className="wind">
+            <p>5mph</p>
+             </div>
+        </div>
+      </div>
     </div>
   );
 }
